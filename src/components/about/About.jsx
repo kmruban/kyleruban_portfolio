a9a153ef2d829c613fe47650cda6e4f35ae8aee4
kmruban <kyleruban1@outlook.com>
2022-05-10 12:21:38 -0400

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,21 @@
+import './about.scss';
+
+function About() {
+  return (
+    <div className='about' id='about'>
+        <div className='container'>
+          <div className='title'>
+            <h1>About Me</h1>
+          </div>
+          <div className='top'>
+            Top Top
+          </div>
+          <div className='bottom'>
+            Bottom
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/data.js
@@ -0,0 +1,96 @@
+  export const webDev = [
+    {
+      id: 1,
+      title: "React",
+      img: "pictures/react.png",
+    },
+    {
+      id: 2,
+      title: "JavaScript",
+      img: "pictures/javascript.png",
+    },
+    {
+      id: 3,
+      title: "HTML 5",
+      img: "pictures/html5.png",
+    },
+    {
+      id: 4,
+      title: "SASS",
+      img: "pictures/sass.png",
+    },
+    {
+      id: 5,
+      title: "CSS",
+      img: "pictures/css.png",
+    },
+
+  ];
+  
+  export const programming = [
+    {
+      id: 1,
+      title: "Java",
+      img: "pictures/java.png",
+    },
+    {
+      id: 2,
+      title: "C#",
+      img: "pictures/csharp.png",
+    },
+  ];
+
+  export const database = [
+    {
+      id: 1,
+      title: "MySql",
+      img: "pictures/mysql.png",
+    },
+    {
+      id: 2,
+      title: "SQL Server",
+      img: "pictures/sqlserver.png",
+    },
+    {
+      id: 3,
+      title: "mongoDB",
+      img: "pictures/mongoDB.png",
+    },
+  ];
+
+  export const graphics = [
+    {
+      id: 1,
+      title: "Web GL",
+      img: "pictures/webgl.png",
+    },
+  ];
+
+
+  export const webProj = [
+    {
+      id: 1,
+      title: "Movies App",
+      img: "pictures/java.png",
+      bio: "bio",
+      link: "https://www.espn.com/mlb/",
+      repo: "",
+    },
+    {
+      id: 2,
+      title: "C#",
+      img: "pictures/csharp.png",
+      bio: "bio",
+      link: "https://www.espn.com/nba/",
+    },
+  ];
+
+  export const graphicsProj = [
+    {
+      id: 1,
+      title: "WebGL Bball",
+      img: "pictures/bball.png",
+      bio: "bio",
+      link: "https://github.com/kyleruban/webGL_BasketballCourt",
+    }
+  ];
